2526f82a5fa76a312ef66e44c7ed77b8ba804d1d
D4ker <Alex-Daker@yandex.ru>
2022-04-20 18:45:29 +0300

    Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>Такой страницы пока что не существует :(</h1>
+    <nuxt-link to="/">Вернуться назад</nuxt-link>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  margin: 0 auto;
+  width: 600px;
+}
+h1 {
+  color: $color-black;
+}
+</style>
